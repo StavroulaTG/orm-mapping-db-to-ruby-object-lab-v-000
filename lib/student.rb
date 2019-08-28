828ba53aed588011001903281298683e8fc883c2
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-28 13:45:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,11 +80,10 @@ class Student
       SELECT *
       FROM students
       WHERE grade < 12
-
     SQL
 
-    DB[:conn].execute(sql, name, grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-      end.first
+      end
     end
 end
