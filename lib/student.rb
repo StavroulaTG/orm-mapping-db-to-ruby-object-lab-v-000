fb55fcd698b6fc8f3b0d561e35b34fc25aa579ff
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-28 14:19:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -117,4 +117,15 @@ class Student
         self.new_from_db(row)
       end.first
     end
+
+    def self.all_students_in_grade_X
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+      LIMIT 1
+
+      SQL
+
+      DB[:conn].execute(sql)
   end
