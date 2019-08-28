14ce54c0db84613769d574332a9ed071a7100bcf
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-28 14:07:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,12 +87,14 @@ class Student
       end
     end
 
-    def self.first_student_in_grade_10
-      sql = <<-SQL
-        SELECT *
-        FROM students
-        WHERE grade = 10
-        ORDER BY students.id LIMIT 1
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      ORDER BY students.id 
+      LIMIT 1
+      
       SQL
 
       DB[:conn].execute(sql).map do |row|
