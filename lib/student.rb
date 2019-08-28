3a8d36d96d3491c1816b41a52be525041a7bd5d9
Stavroula Guerrero <4stacyg@optonline.net>
2019-08-28 13:30:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,9 +75,9 @@ class Student
     DB[:conn].execute(sql)
   end
 
-    def self.students_below_12th_grade
-      sql = "SELECT * FROM students WHERE grade<12;"
-      DB[:conn].execute(sql)
-    end
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade<12;"
+
+    DB[:conn].execute(sql)
   end
 end
